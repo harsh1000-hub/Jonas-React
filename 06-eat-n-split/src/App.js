c1f718937b42bc9f462559c9d6761e9d43879fa0
Harsh Vardhan <72147432+harsh1000-hub@users.noreply.github.com>
2024-02-05 17:47:07 +0530

    Complete eat-n-split project

--- 06-eat-n-split/src/App.js
+++ 06-eat-n-split/src/App.js
@@ -1,4 +1,9 @@
 import { useState } from "react";
+import FriendsList from "./components/FriendsList";
+import FormAddFriend from "./components/FormAddFriend";
+import FormSplitBill from "./components/FormSplitBill";
+import Button from "./components/Button";
+
 const initialFriends = [
   {
     id: 118836,
@@ -27,6 +32,9 @@ function App() {
   // create a common state that is used in FriendsList and FormAddFriend component
   const [friends, setFriends] = useState(initialFriends);
 
+  // create a common state that is used in FriendList and Split Bill
+  const [selectedFriend, setSelectedFriend] = useState(null);
+
   // update the state for setShowAddFriend
   function handleSetShowAddFriend() {
     setShowAddFriend((item) => !item);
@@ -39,10 +47,36 @@ function App() {
     setShowAddFriend(false);
   }
 
+  // update the state for setSelectedFriend
+  function handleSelectedFriend(friend) {
+    setSelectedFriend((currentSelectedFriend) =>
+      currentSelectedFriend?.id === friend.id ? null : friend
+    );
+    setShowAddFriend(false);
+  }
+
+  // handleSplitBill function
+  function handleSplitBill(value) {
+    // console.log(value);
+    setFriends((friends) =>
+      friends.map((friend) =>
+        friend.id === selectedFriend.id
+          ? { ...friend, balance: friend.balance + value }
+          : friend
+      )
+    );
+    // reset the selectedFriend state after split operation done
+    setSelectedFriend(null);
+  }
+
   return (
     <div className="app">
       <div className="sidebar">
-        <FriendsList friends={friends} />
+        <FriendsList
+          friends={friends}
+          onSelectionFriend={handleSelectedFriend}
+          currentlySelectedFriend={selectedFriend}
+        />
         {showAddFriend === true && (
           <FormAddFriend onAddFriend={handleAddFriend} />
         )}
@@ -51,123 +85,15 @@ function App() {
           {showAddFriend === false ? "Add Friend" : "Close"}
         </Button>
       </div>
-      <FormSplitBill />
+      {selectedFriend !== null && (
+        <FormSplitBill
+          selectedFriend={selectedFriend}
+          onSplitBill={handleSplitBill}
+        />
+      )}
+      {/* above line conditionally render the FormSplitBoll when user click on select a particular friends */}
     </div>
   );
 }
 
-// Button Component
-function Button({ children, onClick }) {
-  return (
-    <button className="button" onClick={onClick}>
-      {children}
-    </button>
-  );
-}
-
-// FriendList Component
-function FriendsList({ friends }) {
-  return (
-    <ul>
-      {friends.map((friend) => (
-        <Friend friend={friend} key={friend.id} />
-      ))}
-    </ul>
-  );
-}
-
-// Specific friend component
-function Friend({ friend }) {
-  return (
-    <li>
-      <img src={friend.image} alt={friend.name} />
-      <h3>{friend.name}</h3>
-      {friend.balance < 0 ? (
-        <p className="red">
-          You owe {friend.name} Rs. {Math.abs(friend.balance)}
-        </p>
-      ) : friend.balance === 0 ? (
-        <p>You and {friend.name} are even</p>
-      ) : (
-        <p className="green">
-          {friend.name} owe you Rs. {friend.balance}
-        </p>
-      )}
-      <Button>Select</Button>
-    </li>
-  );
-}
-
-// FormAdd Friend Component
-function FormAddFriend({ onAddFriend }) {
-  // create a state for name and image
-  const [name, setName] = useState("");
-  const [image, setImage] = useState("https://i.pravatar.cc/48");
-
-  // function handleSubmit when form submit
-  function handleSubmit(e) {
-    e.preventDefault(); // prevent the page reload situation when form submit
-
-    // handle edge case if user not fill any of one field then not submitting the form
-    if (!name || !image) return;
-
-    // using a random id generate function
-    const id = crypto.randomUUID();
-
-    // create newFriend object
-    const newFriend = {
-      id,
-      name,
-      image: `${image}?=${id}`,
-      balance: 0,
-    };
-
-    // here we update the state of setFriends
-    onAddFriend(newFriend);
-
-    // reset the state setName and setImage after submitting the form
-    setName("");
-    setImage("https://i.pravatar.cc/48");
-  }
-
-  return (
-    <form className="form-add-friend" onSubmit={handleSubmit}>
-      <label>👬🏻 Friend name </label>
-      <input
-        type="text"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
-      <label>🖼️ Image URL</label>
-      <input
-        type="text"
-        value={image}
-        onChange={(e) => setImage(e.target.value)}
-      />
-      <Button>Add</Button>
-    </form>
-  );
-}
-
-// Form Split Bill Component
-function FormSplitBill() {
-  return (
-    <form className="form-split-bill">
-      <h2>Split a bill with X</h2>
-      <label>💰 Bill Value</label>
-      <input type="text" />
-      <label>🧍‍♂️ Your Expense</label>
-      <input type="text" />
-      <label>👬🏻 X's Expense</label>
-      <input type="text" disabled />{" "}
-      {/* disabled attribute disable the typing in in this input box  */}
-      <label>🤑 Who is paying the bill</label>
-      <select>
-        <option value="user">You</option>
-        <option value="friend">Friend</option>
-      </select>
-      <Button>Split Bill</Button>
-    </form>
-  );
-}
 export default App;
